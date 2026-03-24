0c4fec439118ac8ed0b79c52e6736bcd57675747
kshapw <kshama.wari@m16labs.com>
2026-03-24 12:24:43 +0530

    log name

--- app/middleware/logging.py
+++ app/middleware/logging.py
@@ -25,7 +25,6 @@ def configure_structlog(log_level: str = "INFO") -> None:
         processors=[
             structlog.contextvars.merge_contextvars,
             structlog.stdlib.add_log_level,
-            structlog.stdlib.add_logger_name,
             structlog.processors.TimeStamper(fmt="iso"),
             structlog.processors.StackInfoRenderer(),
             structlog.processors.format_exc_info,
